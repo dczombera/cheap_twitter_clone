cbdd75756ddb87546d00a48146c4fc99474e3615
Dennis Czombera <dennis.czombera@minodes.com>
2015-03-24 22:43:11 +0100

    Add log in/log out mechanism with additional remember me option

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,23 +4,24 @@ class UsersController < ApplicationController
   end
 
   def show
-	@user = User.find(params[:id])
+	  @user = User.find(params[:id])
   end
 
   def create
-	@user = User.new(user_params)
-	if @user.save
-		flash[:success] = "Welcome to the Matrix, #{@user.name}"
-		redirect_to @user
-	else
-		render 'new'
-	end
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome to the Matrix, #{@user.name}"
+      log_in @user
+      redirect_to @user
+    else
+      render 'new'
+    end
   end
 
   private
 
   	def user_params
-		params.require(:user).permit(:name, :email, :password, 
+		  params.require(:user).permit(:name, :email, :password,
 										:password_confirmation)
   	end
 end
